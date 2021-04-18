d77645c7a24d548209f06b77ff0840d0a2bc114f
luisaandreamg <56168185+luisaandreamg@users.noreply.github.com>
2021-04-17 23:42:08 -0500

    Update and rename src/principal/Java/cursoDAgil/bd/cartógrafos/ClienteMapper.java to src/main/java/cursoDAgil/bd/mappers/ClienteMapper.java

--- src/main/java/cursoDAgil/bd/mappers/ClienteMapper.java
+++ src/main/java/cursoDAgil/bd/mappers/ClienteMapper.java
@@ -1,10 +1,15 @@
 package cursoDAgil.bd.mappers;
 
 import java.util.List;
+import java.util.Map;
 
 import cursoDAgil.bd.domain.Cliente;
 
 public interface ClienteMapper {
 	List<Cliente> listarTodosClientes();
-
+	Integer nuevoCliente(Cliente cliente);
+	Integer editarCliente(Cliente cliente);
+	Cliente obtenerClientePorId(Map<String, Integer> mapCliente);
+	Integer eliminarCliente(Map<String, Integer> mapCliente);
 }
+
